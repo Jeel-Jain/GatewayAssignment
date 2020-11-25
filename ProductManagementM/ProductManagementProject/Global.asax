f4a56f70cb7e879c7d5038f0f3792172f3415110
Jeel-Jain <43603514+Jeel-Jain@users.noreply.github.com>
2020-11-25 09:58:47 +0530

    New Assignment Added

--- ProductManagementM/ProductManagementProject/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProductManagementProject.MvcApplication" Language="C#" %>
---
+++ SourceControlAssignment1/SourceControlAssignment1/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceControlAssignment1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View("Index");
+        }
+    }
+}
