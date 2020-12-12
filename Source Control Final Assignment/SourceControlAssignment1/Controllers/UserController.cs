e177043bbd3adcdc83c655378e88a262b0bd29b8
Jeel-Jain <43603514+Jeel-Jain@users.noreply.github.com>
2020-12-12 15:08:29 +0530

    Source control final assignment

---
+++ Source Control Final Assignment/SourceControlAssignment1/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using SourceControlAssignment1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SourceControlAssignment1.Controllers
+{
+    public class UserController : Controller
+    {
+        ProductEntities1 dbObj = new ProductEntities1();
+
+      
+
+        // GET: User
+        public ActionResult User()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [HandleError]
+        public ActionResult Add(tbl_user model)
+        {
+            HttpResponseMessage response = GlobleVariables.WebApiClient.PostAsJsonAsync("User", model).Result;
+            TempData["successMesg"] = "Registration  Successfully";
+            return RedirectToAction("Login", "Login", new { area = "" });
+
+        }
+
+
+
+    }
+}
