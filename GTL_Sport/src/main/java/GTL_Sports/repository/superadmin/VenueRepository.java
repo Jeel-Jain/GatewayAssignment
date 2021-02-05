3aacb2723c74e0e12241a9b7f886f721d6e0cf1b
Jeel-Jain <43603514+Jeel-Jain@users.noreply.github.com>
2021-02-05 09:49:16 +0530

    removed

--- GTL_Sport/src/main/java/GTL_Sports/repository/superadmin/VenueRepository.java
+++
@@ -1,9 +0,0 @@
-package GTL_Sports.repository.superadmin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import GTL_Sports.domain.superadmin.Venue;
-
-public interface VenueRepository extends JpaRepository<Venue, Long> {
-
-}
